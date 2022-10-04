1727eb5bedc25bece51a7904946ee355113c9302
Németh Csaba <nemeth.csaba.tibor@gmail.com>
2022-10-04 20:37:57 +0200

    Created routes for customers and english

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,9 +14,28 @@
 */
 
 //Auth Controller
-$router->post('/login',['uses' => 'AuthController@login']);
-$router->post('/register',['uses' => 'AuthController@register']);
+$router->post('/api/login',['uses' => 'AuthController@login']);
+$router->post('/api/register',['uses' => 'AuthController@register']);
 
 $router->get('/', ['middleware' => 'auth',function () use ($router) {
     return $router->app->version();
 }]);
+
+$router->group(['middleware' => 'auth','prefix' => 'api'],function() use ($router)
+{
+
+    //Customers
+    $router->post('/customer',['uses' => 'CustomerController@create']);
+    $router->get('/customer',['uses' => 'CustomerController@read']);
+    $router->get('/customer/{id}',['uses' => 'CustomerController@read']);
+    $router->patch('/customer/{id}',['uses' => 'CustomerController@update']);
+    $router->delete('/customer/{id}',['uses' => 'CustomerController@delete']);
+
+    //English test
+    $router->post('/english',['uses' => 'EnglishTestController@create']);
+    $router->get('/english',['uses' => 'EnglishTestController@read']);
+    $router->get('/english/{id}',['uses' => 'EnglishTestController@read']);
+    $router->patch('/english/{id}',['uses' => 'EnglishTestController@update']);
+    $router->delete('/english/{id}',['uses' => 'EnglishTestController@delete']);
+
+});
